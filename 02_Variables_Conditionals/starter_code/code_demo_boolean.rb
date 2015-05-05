2222b4a0d31f5952b53c68e584de9e6b389f4e8e
Kisha Richardson <kisha@spunklabs.com>
2015-05-04 20:53:19 -0700

    solutions for boolean

--- 02_Variables_Conditionals/starter_code/code_demo_boolean.rb
+++ 02_Variables_Conditionals/starter_code/code_demo_boolean.rb
@@ -1,7 +1,7 @@
-#Class Labe - TIME: ~ 30 min
+#Class Lab - TIME: ~ 30 min
 #Boolean & Conditionals - Student's File
   #Transform each statement into a method.
-  #Each method should you a conditional to determine if the statment is true/false
+  #Use a conditional to determine if the statment is true/false
   #Each method should provide a "response" with the correct answer
 
 #Check out this example Example
@@ -10,28 +10,88 @@ def compare_two_numbers
   if 7 > 8
     puts "Yes, 7 is greater than 8"
   else
-    puts "No, 7 is not greate than 8"
+    puts "No, 7 is not greater than 8"
   end
 end
 
 ### #1
-puts "Is 8 x 77 greater than 600?"
+
+def compare_to_six_hundred
+  puts "Is 8 x 77 greater than 600?\n"
+  result = 8 * 77
+  if result > 600
+    puts "Yes, #{result} is > than 600"
+  else
+    puts "Nope, #{result} is < than 600"
+  end
+end
 
 ### #2
-puts "Is 1 equal to (7 - 6)?"
+def compare_after_subtracting
+  puts "Is 1 equal to (7 - 6)?"
+  result = 7 - 6
+  if result == 1
+    puts "The numbers are equal"
+  else
+    puts "The numbers are not equal"
+  end
+end
 
 ## 3
-puts "Is 77 greater than 50 AND (88 / 3) less than 30?"
+def compare_number_after_division
+  puts "Is 77 greater than 50 AND (88 / 3) less than 30?"
+  result_1 = 50
+  result_2 = 88/3
+  if 77 > result_1 && result_2 < 30
+    puts "Both conditionals are cool!"
+  else
+    puts "One or more of the conditionals are not cool!"
+  end
+end
 
 ## 4
-puts "Is the length of the word 'wheelbarrow' more than 10 characters long?"
+def determine_length_of_string
+  puts "Is the length of the word 'wheelbarrow' more than 10 characters long?"
+  if "wheelbarrow".length > 10
+    puts "There are more than 10 characters"
+  else
+    puts "There are less than 10 characters"
+  end
+end
 
 ## 5
-puts "Are the amount of seconds in an hour greater than or equal to 3000?"
+def seconds_to_hours
+  puts "Are the amount of seconds in an hour greater than or equal to 3000?"
+  seconds_in_an_hour = 60 * 60
+  if seconds_in_an_hour > 3000
+    puts "Yup, there are more than 3,000 seconds in an hour"
+  else
+    puts "Nope, there are less than 3,000 seconds in a n hour"
+  end
+end
 
 ## 6
-puts "Does the word 'slaughter' include the word laughter?"
+def include_another_word
+  puts "Does the word 'slaughter' include the word laughter?"
+  if "slaughter".include?("laughter")
+    puts "Laughter is in the word slaughter"
+  else
+    puts "Nope it isn't"
+  end
+end
 
-
+puts "***********************METHODS BEING CALLED*************************************"
 #call each method
 compare_two_numbers
+puts "****next method ***"
+compare_to_six_hundred
+puts "****next method ***"
+compare_after_subtracting
+puts "****next method ***"
+compare_number_after_division
+puts "****next method ***"
+determine_length_of_string
+puts "****next method ***"
+seconds_to_hours
+puts "****next method ***"
+include_another_word
