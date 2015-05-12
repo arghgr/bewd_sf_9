dd3e4bb9abdf9c9b87193b12b89d53128c749e38
Kisha Richardson <kisha@spunklabs.com>
2015-05-11 21:43:17 -0700

    udpates

--- 04_Collections_APIs/starter_code/ex_teddit_api_news.rb
+++ 04_Collections_APIs/starter_code/ex_teddit_api_news.rb
@@ -1,7 +1,7 @@
 #Add remote data source and pull in stories from Reddit, Mashable and Digg.
   # http://mashable.com/stories.json
-  # http://digg.com/api/news/popular.json
   #http://www.reddit.com/r/aww/.json
+  #FORGET DIG. Updates
 # These stories will also be upvoted based on our rules. No more user input!
 
 # Pull the json, parse it and then make a new story hash out of each story(Title, Category, Upvotes)
